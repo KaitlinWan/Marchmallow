f368c431d3ac5ababe3a7c320dc747c23c8c60bb
Alan Wang <33503035+alanwang67@users.noreply.github.com>
2018-03-13 13:38:06 -0400

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -1,29 +1,5 @@
-//Alan Wang
-//APCS2 pd08
-//HW18 -- QuickSort
-//2018-03-13t
-  
-/*****************************************************
- * class QuickSort
- * Implements quicksort algo to sort an array of ints in place
- *
- * 1. Summary of QuickSort algorithm:
- * QSort(arr): 
- * QuickSort moves first element to the correct position and partitions the 
-   other elements. Next it repeats the previous step for both left and right 
-   sections until the recursive subcall has only 1 element. 
- * 2a. Worst pivot choice and associated runtime: 
- * 0,O(n^2)
- * 2b. Best pivot choice and associated runtime:
- * arr.length / 2, O(nlogn)
- * 3. Approach to handling duplicate values in array:
- * In the partition code arr[i] < v so the condition is not met and the elements
-   are not swapped. 
- *****************************************************/
-
 public class QuickSort
 {
-    //--------------v  HELPER METHODS  v--------------
     //swap values at indices x, y in array o
     public static void swap( int x, int y, int[] o ) {
 	int tmp = o[x];
@@ -83,8 +59,6 @@ public class QuickSort
         }
 	
     }
-    //--------------^  HELPER METHODS  ^--------------
-
 
 
     /*****************************************************
@@ -96,55 +70,9 @@ public class QuickSort
 	qsortH(d, 0, d.length - 1);
     }
 
-    //you may need a helper method...
-
-
     //main method for testing
     public static void main( String[] args )
     {
-
-	//get-it-up-and-running, static test case:
-	int [] arr1 = {7,1,5,12,3};
-	System.out.println("\narr1 init'd to: " );
-	printArr(arr1);
-	qsort( arr1 );	
-	System.out.println("arr1 after qsort: " );
-	printArr(arr1);
-	// randomly-generated arrays of n distinct vals
-	int[] arrN = new int[10];
-	for( int i = 0; i < arrN.length; i++ )
-	arrN[i] = i;
-       
-	System.out.println("\narrN init'd to: " );
-	printArr(arrN);
-	shuffle(arrN);
-	System.out.println("arrN post-shuffle: " );
-	printArr(arrN);
-	qsort( arrN );
-	System.out.println("arrN after sort: " );
-	printArr(arrN);
-
-	
-	//get-it-up-and-running, static test case w/ dupes:
-	int [] arr2 = {7,1,5,12,3,7};
-	System.out.println("\narr2 init'd to: " );
-	printArr(arr2);
-	qsort( arr2 );	
-	System.out.println("arr2 after qsort: " );
-	printArr(arr2);
-	// arrays of randomly generated ints
-	int[] arrMatey = new int[20];
-	for( int i = 0; i < arrMatey.length; i++ )
-	arrMatey[i] = (int)( 48 * Math.random() );
-       
-	System.out.println("\narrMatey init'd to: " );
-	printArr(arrMatey);
-	shuffle(arrMatey);
-	System.out.println("arrMatey post-shuffle: " );
-	printArr(arrMatey);
-	qsort( arrMatey );
-	System.out.println("arrMatey after sort: " );
-	printArr(arrMatey);
 
     }//end main
 
