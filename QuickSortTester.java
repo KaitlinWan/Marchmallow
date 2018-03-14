aaf03b2b21db1e37a630d760e07390c99759be3b
Kaitlin <kaitlin.wan@gmail.com>
2018-03-14 01:43:22 -0400

    checked + fixed

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,4 +1,5 @@
 import quicksort.*;
+import java.util.Scanner;
 
 public class QuickSortTester {
 
@@ -10,86 +11,88 @@ public class QuickSortTester {
      * postcond: arr is a best case scenario for quicksort
      */
     public static void bestCaseGen(int[] arr, int low, int high) {
-	int count = high - 1 - low;
-	if (count < 2)
-	    return;
+      int count = high - 1 - low;
+      if (count < 2){
+        return;
+      }
 
-	int mid = low + count / 2; //pivot
+      int mid = low + count / 2; //pivot
+      //recurse through left side
+      bestCaseGen(arr, low, mid);
+      //swap pivot
+      QuickSort.swap(low, mid, arr);
 
-	//recurse through left side
-	bestCaseGen(arr, low, mid);
-
-	//swap pivot
-	QuickSort.swap(low, mid, arr);
-
-	//recurse through right side
-	bestCaseGen(arr, mid + 1, high);
+      //recurse through right side
+      bestCaseGen(arr, mid + 1, high);
     }
 
     public static void main(String[] args) {
-	long a = 0; //begin time
-	long b = 0; //end time
+      long a = 0; //begin time
+      long b = 0; //end time
 
-	//worst case
-	for (int n = 30000; n >= 5000; n -= 100) { //array size
-	    System.out.print(n + ","); //print array size
-	    long total = 0; //total runtime for each run
+      //worst case
+      for (int n = 30000; n >= 5000; n -= 100) { //array size
+        System.out.print(n + ","); //print array size
+        long total = 0; //total runtime for each run
 
-	    //declare and instantiate
-	    int[] arr = new int[n];
-	    for (int i = 0; i < n; i++)
-		arr[i] = i;
+        //declare and instantiate
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+          arr[i] = i;
+        }
 
-	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
-		if (trial > 99) { //in case there is weird behavior for first few runs
-		    a = System.currentTimeMillis();
-		    QuickSort.qsort(arr); //quicksort
-		    b = System.currentTimeMillis();
-		    total += b - a; //find time took and add to total
-		}
-	    }
-	    System.out.println(total / 4900000.0); //average time for one search
-	}
+        for (int trial = 0; trial < 5000; trial++) { //4900 runs
+          if (trial > 99) { //in case there is weird behavior for first few runs
+            a = System.currentTimeMillis();
+            QuickSort.qsort(arr); //quicksort
+            b = System.currentTimeMillis();
+            total += b - a; //find time took and add to total
+          }
+        }
+        System.out.println(total / 4900000.0); //average time for one search
+      }
 
-	//best case
-	for (int n = 30000; n >= 5000; n -= 100) { //array size
-	    System.out.print(n + ","); //print array size
-	    long total = 0; //total runtime for each run
+      //best case
 
-	    //declare and instantiate
-	    int[] arr = new int[n];
-	    for (int i = 0; i < n; i++)
-		arr[i] = i;
+      for (int n = 30000; n >= 5000; n -= 100) { //array size
+        System.out.print(n + ","); //print array size
+        long total = 0; //total runtime for each run
 
-	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
-		if (trial > 99) { //in case there is weird behavior for first few runs
-		    bestCaseGen(arr, 0, arr.length);
-		    a = System.currentTimeMillis();
-		    QuickSort.qsort(arr); //quicksort
-		    b = System.currentTimeMillis();
-		    total += b - a; //find time took and add to total
-		}
-	    }
-	    System.out.println(total / 4900000.0); //average time for one search
-	}
+        //declare and instantiate
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+          arr[i] = i;
+        }
 
-	//average case
-	for (int n = 30000; n >= 5000; n -= 100) { //array size
-	    System.out.print(n + ","); //print array size
-	    long total = 0; //total runtime for each run
+        for (int trial = 0; trial < 5000; trial++) { //4900 runs
+          if (trial > 99) { //in case there is weird behavior for first few runs
+            bestCaseGen(arr, 0, arr.length);
+            a = System.currentTimeMillis();
+            QuickSort.qsort(arr); //quicksort
+            b = System.currentTimeMillis();
+            total += b - a; //find time took and add to total
+          }
+        }
+        System.out.println(total / 4900000.0); //average time for one search
+      }
 
-	    //declare and instantiate
-	    int[] arr = QuickSort.buildArray(n, n);
+      //average case
 
-	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
-		if (trial > 99) { //in case there is weird behavior for first few runs
-		    a = System.currentTimeMillis();
-		    QuickSort.qsort(arr); //quicksort
-		    b = System.currentTimeMillis();
-		    total += b - a; //find time took and add to total
-		}
-	    }
-	    System.out.println(total / 4900000.0); //average time for one search
-	}
+      for (int n = 30000; n >= 5000; n -= 100) { //array size
+        System.out.print(n + ","); //print array size
+        long total = 0; //total runtime for each run
+
+        //declare and instantiate
+        int[] arr = QuickSort.buildArray(n, n);
+
+        for (int trial = 0; trial < 5000; trial++) { //4900 runs
+          if (trial > 99) { //in case there is weird behavior for first few runs
+            a = System.currentTimeMillis();
+            QuickSort.qsort(arr); //quicksort
+            total += b - a; //find time took and add to total
+          }
+        }
+        System.out.println(total / 4900000.0); //average time for one search
+      }
     }
-}
+  }
