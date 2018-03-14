7f376fcf4ec0a2e70ce5697c88df1ad0d04bb0f4
Kevin <kwang11@stuy.edu>
2018-03-13 21:32:49 -0400

    commented out shuffle

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -2,94 +2,94 @@ import quicksort.*;
 
 public class QuickSortTester {
 
-	/** Generates a best case scenario for quicksort by working backwards
-	 * @param arr is the array
-	 * @param low is the lower index of the array
-	 * @param high is the length of the array
-	 * precond: arr is an array sorted in ascending order
-	 * postcond: arr is a best case scenario for quicksort
-	 */
-	public static void bestCaseGen(int[] arr, int low, int high) {
-		int count = high - 1 - low;
-		if (count < 2)
-			return;
+    /** Generates a best case scenario for quicksort by working backwards
+     * @param arr is the array
+     * @param low is the lower index of the array
+     * @param high is the length of the array
+     * precond: arr is an array sorted in ascending order
+     * postcond: arr is a best case scenario for quicksort
+     */
+    public static void bestCaseGen(int[] arr, int low, int high) {
+	int count = high - 1 - low;
+	if (count < 2)
+	    return;
 
-		int mid = low + count / 2; //pivot
+	int mid = low + count / 2; //pivot
 
-		//recurse through left side
-		bestCaseGen(arr, low, mid);
+	//recurse through left side
+	bestCaseGen(arr, low, mid);
 
-		//swap pivot
-		QuickSort.swap(low, mid, arr);
+	//swap pivot
+	QuickSort.swap(low, mid, arr);
 
-		//recurse through right side
-		bestCaseGen(arr, mid + 1, high);
+	//recurse through right side
+	bestCaseGen(arr, mid + 1, high);
+    }
+
+    public static void main(String[] args) {
+	long a = 0; //begin time
+	long b = 0; //end time
+
+	//worst case
+	for (int n = 17000; n >= 5000; n -= 100) { //array size
+	    System.out.print(n + ","); //print array size
+	    long total = 0; //total runtime for each run
+
+	    //declare and instantiate
+	    int[] arr = new int[n];
+	    for (int i = 0; i < n; i++)
+		arr[i] = i;
+
+	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
+		if (trial > 99) { //in case there is weird behavior for first few runs
+		    a = System.currentTimeMillis();
+		    QuickSort.qsort(arr); //quicksort
+		    b = System.currentTimeMillis();
+		    total += b - a; //find time took and add to total
+		}
+	    }
+	    System.out.println(total / 4900000.0); //average time for one search
 	}
 
-	public static void main(String[] args) {
-		long a = 0; //begin time
-		long b = 0; //end time
+	//best case
+	for (int n = 17000; n >= 5000; n -= 100) { //array size
+	    System.out.print(n + ","); //print array size
+	    long total = 0; //total runtime for each run
 
-		//worst case
-		for (int n = 17000; n >= 5000; n -= 100) { //array size
-			System.out.print(n + ","); //print array size
-			long total = 0; //total runtime for each run
+	    //declare and instantiate
+	    int[] arr = new int[n];
+	    for (int i = 0; i < n; i++)
+		arr[i] = i;
 
-			//declare and instantiate
-			int[] arr = new int[n];
-			for (int i = 0; i < n; i++)
-				arr[i] = i;
-
-			for (int trial = 0; trial < 5000; trial++) { //4900 runs
-				if (trial > 99) { //in case there is weird behavior for first few runs
-					a = System.currentTimeMillis();
-					QuickSort.qsort(arr); //quicksort
-					b = System.currentTimeMillis();
-					total += b - a; //find time took and add to total
-				}
-			}
-			System.out.println(total / 4900000.0); //average time for one search
+	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
+		if (trial > 99) { //in case there is weird behavior for first few runs
+		    bestCaseGen(arr, 0, arr.length);
+		    a = System.currentTimeMillis();
+		    QuickSort.qsort(arr); //quicksort
+		    b = System.currentTimeMillis();
+		    total += b - a; //find time took and add to total
 		}
-
-		//best case
-		for (int n = 17000; n >= 5000; n -= 100) { //array size
-			System.out.print(n + ","); //print array size
-			long total = 0; //total runtime for each run
-
-			//declare and instantiate
-			int[] arr = new int[n];
-			for (int i = 0; i < n; i++)
-				arr[i] = i;
-
-			for (int trial = 0; trial < 5000; trial++) { //4900 runs
-				if (trial > 99) { //in case there is weird behavior for first few runs
-					bestCaseGen(arr, 0, arr.length);
-					a = System.currentTimeMillis();
-					QuickSort.qsort(arr); //quicksort
-					b = System.currentTimeMillis();
-					total += b - a; //find time took and add to total
-				}
-			}
-			System.out.println(total / 4900000.0); //average time for one search
-		}
-
-		//average case
-		for (int n = 17000; n >= 5000; n -= 100) { //array size
-			System.out.print(n + ","); //print array size
-			long total = 0; //total runtime for each run
-
-			//declare and instantiate
-			int[] arr = QuickSort.buildArray(n, n);
-
-			for (int trial = 0; trial < 5000; trial++) { //4900 runs
-				if (trial > 99) { //in case there is weird behavior for first few runs
-					a = System.currentTimeMillis();
-					QuickSort.qsort(arr); //quicksort
-					b = System.currentTimeMillis();
-					total += b - a; //find time took and add to total
-				}
-			}
-			System.out.println(total / 4900000.0); //average time for one search
-		}
+	    }
+	    System.out.println(total / 4900000.0); //average time for one search
 	}
+
+	//average case
+	for (int n = 17000; n >= 5000; n -= 100) { //array size
+	    System.out.print(n + ","); //print array size
+	    long total = 0; //total runtime for each run
+
+	    //declare and instantiate
+	    int[] arr = QuickSort.buildArray(n, n);
+
+	    for (int trial = 0; trial < 5000; trial++) { //4900 runs
+		if (trial > 99) { //in case there is weird behavior for first few runs
+		    a = System.currentTimeMillis();
+		    QuickSort.qsort(arr); //quicksort
+		    b = System.currentTimeMillis();
+		    total += b - a; //find time took and add to total
+		}
+	    }
+	    System.out.println(total / 4900000.0); //average time for one search
+	}
+    }
 }
--- quicksort/QuickSort.java
+++ quicksort/QuickSort.java
@@ -1,10 +1,10 @@
 package quicksort;
 
 /*
-Kaitlin Wan, Alan Wang, Kevin Wang
-APCS2 Pd. 8
-HW #18 -- QuickSort
-2018-03-13
+  Kaitlin Wan, Alan Wang, Kevin Wang
+  APCS2 Pd. 8
+  HW #18 -- QuickSort
+  2018-03-13
 */
 
 /*****************************************************
@@ -29,154 +29,154 @@ HW #18 -- QuickSort
  *****************************************************/
 
 public class QuickSort {
-	//--------------v  HELPER METHODS  v--------------
-	//swap values at indices x, y in array o
-	public static void swap(int x, int y, int[] o) {
-		int tmp = o[x];
-		o[x] = o[y];
-		o[y] = tmp;
+    //--------------v  HELPER METHODS  v--------------
+    //swap values at indices x, y in array o
+    public static void swap(int x, int y, int[] o) {
+	int tmp = o[x];
+	o[x] = o[y];
+	o[y] = tmp;
+    }
+
+    //print input array 
+    public static void printArr(int[] a) {
+	for (int o : a)
+	    System.out.print(o + " ");
+	System.out.println();
+    }
+
+    //shuffle elements of input array
+    public static void shuffle(int[] d) {
+	int tmp;
+	int swapPos;
+	for (int i = 0; i < d.length; i++) {
+	    tmp = d[i];
+	    swapPos = i + (int) ((d.length - i) * Math.random());
+	    swap(i, swapPos, d);
+	}
+    }
+
+    //return int array of size s, with each element fr range [0,maxVal)
+    public static int[] buildArray(int s, int maxVal) {
+	int[] retArr = new int[s];
+	for (int i = 0; i < retArr.length; i++)
+	    retArr[i] = (int) (maxVal * Math.random());
+	return retArr;
+    }
+
+    /**
+     * Method partition moves all values less than a particular set value (the pivot) to one
+     * side
+     * @param arr is the array to be operated on
+     * @param low is the index of the lower bound of the array that you want to operate on
+     * @param high is the index of the upper bound of the array that you want to operate on
+     * @param pivotPos is the index of the pivot value
+     */
+    public static int[] partition(int[] arr, int low, int high, int pivotPos) {
+	if (low >= high) //if arr.length <= 1
+	    return arr;
+
+	int pivot = arr[pivotPos];
+
+	//swap pivot with rightmost element
+	swap(pivotPos, high, arr);
+
+	int pivotFinal = low;
+	for (int i = low; i < high; i++) {
+	    if (arr[i] < pivot) { //move values smaller than pivot to one side
+
+		//swap
+		swap(pivotFinal, i, arr);
+		pivotFinal += 1; //update pivot's final index
+	    }
 	}
 
-	//print input array 
-	public static void printArr(int[] a) {
-		for (int o : a)
-			System.out.print(o + " ");
-		System.out.println();
-	}
+	//swap pivot to correct index
+	swap(pivotFinal, high, arr);
 
-	//shuffle elements of input array
-	public static void shuffle(int[] d) {
-		int tmp;
-		int swapPos;
-		for (int i = 0; i < d.length; i++) {
-			tmp = d[i];
-			swapPos = i + (int) ((d.length - i) * Math.random());
-			swap(i, swapPos, d);
-		}
-	}
+	partition(arr, low, pivotFinal - 1, (pivotFinal - 1 + low) / 2); //run on lower half
+	partition(arr, pivotFinal + 1, high, (high + pivotFinal + 1) / 2); //run on upper half
+	return arr;
+    }
+    //--------------^  HELPER METHODS  ^--------------
 
-	//return int array of size s, with each element fr range [0,maxVal)
-	public static int[] buildArray(int s, int maxVal) {
-		int[] retArr = new int[s];
-		for (int i = 0; i < retArr.length; i++)
-			retArr[i] = (int) (maxVal * Math.random());
-		return retArr;
-	}
+    /*****************************************************
+     * void qsort(int[])
+     * @param d -- array of ints to be sorted in place
+     *****************************************************/
+    public static void qsort(int[] d) {
+	//shuffle(d); //shuffle d first in case it is nearly sorted to prevent worst case
+	partition(d, 0, d.length - 1, 0); //use first element as pivot
+    }
 
-	/**
-	* Method partition moves all values less than a particular set value (the pivot) to one
-	* side
-	* @param arr is the array to be operated on
-	* @param low is the index of the lower bound of the array that you want to operate on
-	* @param high is the index of the upper bound of the array that you want to operate on
-	* @param pivotPos is the index of the pivot value
-	*/
-	public static int[] partition(int[] arr, int low, int high, int pivotPos) {
-		if (low >= high) //if arr.length <= 1
-			return arr;
+    //main method for testing
+    public static void main(String[] args) {
 
-		int pivot = arr[pivotPos];
+	//get-it-up-and-running, static test case:
+	int[] arr1 = { 7, 1, 5, 12, 3 };
+	System.out.println("\narr1 init'd to: ");
+	printArr(arr1);
 
-		//swap pivot with rightmost element
-		swap(pivotPos, high, arr);
+	qsort(arr1);
+	System.out.println("arr1 after qsort: ");
+	printArr(arr1);
 
-		int pivotFinal = low;
-		for (int i = low; i < high; i++) {
-			if (arr[i] < pivot) { //move values smaller than pivot to one side
+	// randomly-generated arrays of n distinct vals
+	int[] arrN = new int[10];
+	for (int i = 0; i < arrN.length; i++)
+	    arrN[i] = i;
 
-				//swap
-				swap(pivotFinal, i, arr);
-				pivotFinal += 1; //update pivot's final index
-			}
-		}
+	System.out.println("\narrN init'd to: ");
+	printArr(arrN);
 
-		//swap pivot to correct index
-		swap(pivotFinal, high, arr);
+	shuffle(arrN);
+	System.out.println("arrN post-shuffle: ");
+	printArr(arrN);
 
-		partition(arr, low, pivotFinal - 1, (pivotFinal - 1 + low) / 2); //run on lower half
-		partition(arr, pivotFinal + 1, high, (high + pivotFinal + 1) / 2); //run on upper half
-		return arr;
-	}
-	//--------------^  HELPER METHODS  ^--------------
+	qsort(arrN);
+	System.out.println("arrN after sort: ");
+	printArr(arrN);
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-	/*****************************************************
-	 * void qsort(int[])
-	 * @param d -- array of ints to be sorted in place
-	 *****************************************************/
-	public static void qsort(int[] d) {
-		shuffle(d); //shuffle d first in case it is nearly sorted to prevent worst case
-		partition(d, 0, d.length - 1, 0); //use first element as pivot
-	}
+	//get-it-up-and-running, static test case w/ dupes:
+	int[] arr2 = { 7, 1, 5, 12, 3, 7 };
+	System.out.println("\narr2 init'd to: ");
+	printArr(arr2);
 
-	//main method for testing
-	public static void main(String[] args) {
+	qsort(arr2);
+	System.out.println("arr2 after qsort: ");
+	printArr(arr2);
 
-		//get-it-up-and-running, static test case:
-		int[] arr1 = { 7, 1, 5, 12, 3 };
-		System.out.println("\narr1 init'd to: ");
-		printArr(arr1);
+	// arrays of randomly generated ints
+	int[] arrMatey = new int[20];
+	for (int i = 0; i < arrMatey.length; i++)
+	    arrMatey[i] = (int) (48 * Math.random());
 
-		qsort(arr1);
-		System.out.println("arr1 after qsort: ");
-		printArr(arr1);
+	System.out.println("\narrMatey init'd to: ");
+	printArr(arrMatey);
 
-		// randomly-generated arrays of n distinct vals
-		int[] arrN = new int[10];
-		for (int i = 0; i < arrN.length; i++)
-			arrN[i] = i;
+	shuffle(arrMatey);
+	System.out.println("arrMatey post-shuffle: ");
+	printArr(arrMatey);
 
-		System.out.println("\narrN init'd to: ");
-		printArr(arrN);
+	qsort(arrMatey);
+	System.out.println("arrMatey after sort: ");
+	printArr(arrMatey);
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-		shuffle(arrN);
-		System.out.println("arrN post-shuffle: ");
-		printArr(arrN);
+	//array with 3 of the same values
+	int[] bobby = { 1, 1, 2, 2, 3, 4, 5, 5, 6, 12, 22, 13, 15, 15, 15 };
+	System.out.println("\nbobby init'd to: ");
+	printArr(bobby);
 
-		qsort(arrN);
-		System.out.println("arrN after sort: ");
-		printArr(arrN);
-		/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
-		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	shuffle(bobby);
+	System.out.println("bobby post-shuffle: ");
+	printArr(bobby);
 
-		//get-it-up-and-running, static test case w/ dupes:
-		int[] arr2 = { 7, 1, 5, 12, 3, 7 };
-		System.out.println("\narr2 init'd to: ");
-		printArr(arr2);
-
-		qsort(arr2);
-		System.out.println("arr2 after qsort: ");
-		printArr(arr2);
-
-		// arrays of randomly generated ints
-		int[] arrMatey = new int[20];
-		for (int i = 0; i < arrMatey.length; i++)
-			arrMatey[i] = (int) (48 * Math.random());
-
-		System.out.println("\narrMatey init'd to: ");
-		printArr(arrMatey);
-
-		shuffle(arrMatey);
-		System.out.println("arrMatey post-shuffle: ");
-		printArr(arrMatey);
-
-		qsort(arrMatey);
-		System.out.println("arrMatey after sort: ");
-		printArr(arrMatey);
-		/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
-		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-		//array with 3 of the same values
-		int[] bobby = { 1, 1, 2, 2, 3, 4, 5, 5, 6, 12, 22, 13, 15, 15, 15 };
-		System.out.println("\nbobby init'd to: ");
-		printArr(bobby);
-
-		shuffle(bobby);
-		System.out.println("bobby post-shuffle: ");
-		printArr(bobby);
-
-		qsort(bobby);
-		System.out.println("bobby after sort: ");
-		printArr(bobby);
-	}//end main
+	qsort(bobby);
+	System.out.println("bobby after sort: ");
+	printArr(bobby);
+    }//end main
 
 }//end class QuickSort
